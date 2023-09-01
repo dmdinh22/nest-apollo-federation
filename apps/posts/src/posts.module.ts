f8cd0b688a6cbad27c014598e90361c979c7c072
David D <hello@davidmdinh.com>
2023-08-31 19:59:42 -0700

    tie posts and users together with federation

--- apps/posts/src/posts.module.ts
+++ apps/posts/src/posts.module.ts
@@ -6,6 +6,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { PostsResolver } from './posts.resolver';
 import { PostsService } from './posts.service';
+import { UsersResolver } from './users.resolver';
 
 @Module({
   imports: [
@@ -16,6 +17,6 @@ import { PostsService } from './posts.service';
       },
     }),
   ],
-  providers: [PostsResolver, PostsService],
+  providers: [PostsResolver, PostsService, UsersResolver],
 })
 export class PostsModule {}
--- apps/users/src/users.resolver.ts
+++ apps/users/src/users.resolver.ts
@@ -1,4 +1,10 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Query,
+  ResolveReference,
+  Resolver,
+} from '@nestjs/graphql';
 import { CreateUserInput } from './dto/create-user.input';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
@@ -20,5 +26,10 @@ export class UsersResolver {
   @Query(() => User, { name: 'user' })
   findOne(@Args('id') id: string) {
     return this.usersService.findOne(id);
+  }
+
+  @ResolveReference()
+  resolveReference(reference: { __typename: string; id: string }): User {
+    return this.usersService.findOne(reference.id);
   }
 }
