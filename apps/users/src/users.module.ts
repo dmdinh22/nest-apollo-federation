e0a22cef4accc78db77fd9a77a3d9341141937cc
David D <hello@davidmdinh.com>
2023-08-31 19:07:22 -0700

    beam me up scotty

---
+++ apps/users/src/users.module.ts
@@ -0,0 +1,21 @@
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UsersResolver } from './users.resolver';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+  ],
+  providers: [UsersResolver, UsersService],
+})
+export class UsersModule {}
